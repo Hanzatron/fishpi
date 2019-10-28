99e5d14f136189342e93a9351e17da37691a57bc
Hanzatron <hans_van_gaveren@hotmail.com>
2019-10-28 13:19:36 +0100

    V2.0

--- fishpi.py
+++ fishpi.py
@@ -2,12 +2,13 @@
 
 #####################################################################################
 # FISHPI																		HANS
-# V1.2
+# V2.0
 #
 # V0.0		21/02/15		herwerkt naar OOP stijl
 # V1.0		22/02/15		Flask webserver toegevoegd
 # V1.1		24/02/15		zonsondergang/opgang toegevoegd
 # V1.2		25/02/15		Debug tijdklok()
+# V2.0		28/10/19		aangepast naar python3
 #####################################################################################
 # Sturing visbak
 # Voor zonsopgang/ondergang wordt PyEphem gebruikt. pip install pyephem
